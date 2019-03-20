e1ebe6d5b71dcc32ee80fe89618d6f3a9041d5bb
mavenraven <developers@mavenraven.org>
2019-03-20 14:35:04 -0400

    jdk12

--- src/main/java/org/mavenraven/App.java
+++ src/main/java/org/mavenraven/App.java
@@ -3,9 +3,14 @@ package org.mavenraven;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
 import com.google.gson.Gson;
+import com.mapbox.api.staticmap.v1.MapboxStaticMap;
+import com.mapbox.api.staticmap.v1.models.StaticMarkerAnnotation;
+import com.mapbox.api.staticmap.v1.models.StaticPolylineAnnotation;
+import com.mapbox.core.utils.MapboxUtils;
 import com.mapbox.geojson.GeoJson;
 import com.mapbox.geojson.LineString;
 import com.mapbox.geojson.Point;
+import okhttp3.HttpUrl;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 
@@ -13,6 +18,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -34,7 +41,16 @@ public class App {
         try {
             Reader in = new FileReader(args.csvFileLocation);
             try {
-                CSVParser parsed = CSVFormat.DEFAULT.parse(in);
+                CSVParser parsed = CSVFormat.DEFAULT.withDelimiter(';').withHeader().parse(in);
+                List<List<Point>> grouped = GroupPoints.exec(parsed.getRecords().stream()
+                        .map(x -> ToPointWithDateTime.exec(x)).collect(Collectors.toList()));
+                List<LineString> lineStrings = grouped.stream().map(x -> LineString.fromLngLats(x))
+                        .collect(Collectors.toList());
+                MapboxStaticMap map = MapboxStaticMap.builder().accessToken(args.mapboxAccessToken)
+                        .geoJson(lineStrings.get(3)).cameraPoint(lineStrings.get(3).coordinates().get(0)).height(1024)
+                        .width(1024).retina(true).cameraAuto(true).logo(false).build();
+                HttpUrl url = map.url();
+                int x = 1;
             } catch (IOException e) {
                 System.err.println("Unable to parse CSV file: " + e.getMessage());
                 System.exit(1);
