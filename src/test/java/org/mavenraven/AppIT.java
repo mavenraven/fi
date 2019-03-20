bd9dfe04e80467562bfb427d5eb93c1594308c27
mavenraven <developers@mavenraven.org>
2019-03-19 20:18:36 -0400

    refactoring

--- src/test/java/org/mavenraven/AppIT.java
+++ src/test/java/org/mavenraven/AppIT.java
@@ -12,23 +12,27 @@ import java.nio.file.Paths;
 
 public class AppIT {
 
-    @Test
-    public void itRunsTheCli() throws IOException {
+    private String jarPath;
+    private String csvFileLocation;
+
+    @BeforeEach
+    public void beforeEach() {
         String buildDir = System.getProperty("buildDirectory");
         String jarName = System.getProperty("jarName");
-        String jarPath = Paths.get(buildDir, jarName).toString();
         String testSourceDir = System.getProperty("testSourceDirectory");
-        String csvFileLocation = Paths.get(testSourceDir, "resources", "gps_dataset.csv").toString();
 
+        jarPath = Paths.get(buildDir, jarName).toString();
+        csvFileLocation = Paths.get(testSourceDir, "resources", "gps_dataset.csv").toString();
+    }
+
+    @Test
+    public void itRunsTheCli() throws IOException {
         Runtime rt = Runtime.getRuntime();
         String[] commands = { "java", "-jar", jarPath, csvFileLocation };
         Process proc = rt.exec(commands);
 
         BufferedReader resultOutput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-
         String line = resultOutput.readLine();
-
         assertEquals(line, "Hello World!");
-
     }
 }
