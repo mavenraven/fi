faa650c458953c42a6410f4dc7cc4c2d6a5213a8
mavenraven <developers@mavenraven.org>
2019-09-29 21:27:44 -0400

    cool

--- haversine.rb
+++ haversine.rb
@@ -3,7 +3,7 @@ require 'bigdecimal'
 require 'bigdecimal/util'
 
 Signal.trap("SIGPIPE", "SYSTEM_DEFAULT") 
-BEGIN { last = nil ; last_walk = 0 }
+BEGIN { last = nil ; last_walk = -1 }
 
 fields = $_.split
 
@@ -14,16 +14,17 @@ walk_number_field = ENV["WALK_FN"].to_i
 walk = fields[walk_number_field]
 
 if !last
-  last = {lat: fields[lat_field].to_d, lon: fields[lon_field].to_d}
+  last = {lat: fields[lat_field].to_f, lon: fields[lon_field].to_f}
 end
 
-lat = fields[lat_field].to_d
-lon = fields[lon_field].to_d
+lat = fields[lat_field].to_f
+lon = fields[lon_field].to_f
 
 haversine = Haversine.distance(lat, lon, last[:lat], last[:lon]) 
-last = {lat: fields[lat_field].to_d, lon: fields[lon_field].to_d}
+last = {lat: fields[lat_field].to_f, lon: fields[lon_field].to_f}
 if last_walk != walk
 	puts "#{$_.strip}#{$;.source}0"
+        last_walk = walk
 else
-	puts "#{$_.strip}#{$;.source}#{haversine.to_mi}"
+	puts "#{$_.strip}#{$;.source}" + sprintf("%.2f", haversine.to_ft)
 end
