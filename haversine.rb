03060fa64f11962898ab721ffb2311d1379576bb
mavenraven <developers@mavenraven.org>
2019-09-29 21:12:42 -0400

    more

--- haversine.rb
+++ haversine.rb
@@ -1,16 +1,29 @@
 require 'haversine'
+require 'bigdecimal'
+require 'bigdecimal/util'
+
 Signal.trap("SIGPIPE", "SYSTEM_DEFAULT") 
-BEGIN { last = nil }
+BEGIN { last = nil ; last_walk = 0 }
 
 fields = $_.split
 
-lat_field = ENV["LAT#{n}_FIELD"].to_i
-lon_field = ENV["LON#{n}_FIELD"].to_i
+lat_field = ENV["LAT_FN"].to_i
+lon_field = ENV["LON_FN"].to_i
+walk_number_field = ENV["WALK_FN"].to_i
+
+walk = fields[walk_number_field]
 
 if !last
-  last = {lat: fields[lat_field}, lon: fields[lon_field]}
+  last = {lat: fields[lat_field].to_d, lon: fields[lon_field].to_d}
 end
 
-haversine = Haversine.distance(fields[lat_field], fields[lon_field], last[:lat], last[:lon]) 
-last = {lat: fields[lat_field}, lon: fields[lon_field]}
-puts "#{$_.strip}#{$;.source}#{haversine}"
+lat = fields[lat_field].to_d
+lon = fields[lon_field].to_d
+
+haversine = Haversine.distance(lat, lon, last[:lat], last[:lon]) 
+last = {lat: fields[lat_field].to_d, lon: fields[lon_field].to_d}
+if last_walk != walk
+	puts "#{$_.strip}#{$;.source}0"
+else
+	puts "#{$_.strip}#{$;.source}#{haversine.to_mi}"
+end
